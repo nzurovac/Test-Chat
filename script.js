cd1ee6dbccf3ca5d9977d3fa4b7f9dd1ee4eef70
nzurovac <pasic.natasa89@gmail.com>
2024-08-25 12:42:00 +0200

    Intial Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('insurance-form');
+const incomeInput = document.getElementById('income');
+const submitButton = document.getElementById('submit');
+const resultElement = document.getElementById('result');
+
+
+document.addEventListener("DOMContentLoaded", function(){
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault(); // prevent form submission
+  const income = parseInt(incomeInput.value.trim(), 10);
+  if (income >= 45000) {
+    const premium = calculatePremium(income);
+      
+        resultElement.textContent = `You are eligible for our insurance! Your premium is: ${premium} kr`;
+        console.log('you are eligible');
+  } else {
+    triggerRule(); // call the function when income is less than 45 000 kr
+  }
+});
+});
+function calculatePremium(income) {
+  // TO DO: implement the premium calculation logic here
+  // for now, let's assume it's a sumple percentage of the income
+  return income * 0.05;
+}
+
+// TO DO: implement the triggerRule function here
+function triggerRule() {
+  // this function shall be called when the income is less than 45,000kr
+  // for now, let's just log a message to the console
+  console.log('Not Eligible for insurance');
+};
+
+if(module.hot){
+  module.hot.accept();
+};
