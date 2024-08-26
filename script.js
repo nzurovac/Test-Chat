40d50cbd73fce363338a5b5563e570be6928a1ab
nzurovac <pasic.natasa89@gmail.com>
2024-08-26 20:44:31 +0200

    upd

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ document.addEventListener("DOMContentLoaded", function() {
       resultNotEligible.textContent = `Please contact us to discuss your insurance options`;
       resultNotEligible.classList.remove('hidden');
       resultElement.classList.add('hidden');
-      triggerPriceNotCalculated();
-      // Hide regular chat
+      // Hide regular chat and trigger PriceNotCalculated chat
       pzl.api.triggerRule({
         ruleId: 'eb1d1fb4-4547-4576-bbbe-5284c7ad88c2',
         force: false
       });
+      triggerPriceNotCalculated();
     } else if (income >= 45000) {
       const premium = calculatePremium(income);
       resultElement.textContent = `You are eligible for our insurance! Your premium is: ${premium} kr`;
@@ -38,10 +38,6 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log('You are eligible');
       // Show regular chat
       triggerRegularChat();
-      pzl.api.triggerRule({
-        ruleId: '5ef4eca5-753c-4959-ad71-3d7a7db7c559',
-        force: false
-      });
     }
   }
 
